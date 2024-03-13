9ee8af7058522681a783ea51e51867910ac2a451
junaeddurjoy <junaed.durjoy@gmail.com>
2024-03-13 15:58:04 +0600

    error handled

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -4,35 +4,38 @@ import useAxiosPublic from "../../hooks/useAxiosPublic";
 import { useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
-    const  {googleSignIn} = useAuth();
+    const { googleSignIn } = useAuth();
     const axiosPublic = useAxiosPublic();
     const navigate = useNavigate();
 
     const handleGoogleSignIn = () => {
         googleSignIn()
-        .then(result => {
-            console.log(result.user);
-            const userInfo = {
-                email: result.user?.email,
-                name: result.user?.displayName
-            }
-            axiosPublic.post('/users', userInfo)
-            .then(res => {
-                console.log(res.data);
-                navigate('/');
+            .then(result => {
+                console.log(result.user);
+                const userInfo = {
+                    email: result.user?.email,
+                    name: result.user?.displayName
+                }
+                axiosPublic.post('/users', userInfo)
+                    .then(res => {
+                        console.log(res.data);
+                        navigate('/');
+                    })
             })
-        })
     }
     return (
-        <div className="p-8 -mt-14">
-            <div className="divider"></div>
-            <div>
-                <button onClick={handleGoogleSignIn} className="btn">
-                    <FaGoogle className="mr-4"></FaGoogle>
-                    Login with Google
-                </button>
+        <>
+
+            <div className="p-8 -mt-14">
+                <div className="divider"></div>
+                <div>
+                    <button onClick={handleGoogleSignIn} className="btn">
+                        <FaGoogle className="mr-7"></FaGoogle>
+                        Login with Google
+                    </button>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,16 +1,15 @@
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import useAuth from "./useAuth";
+// import { useNavigate } from "react-router-dom";
+// import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: import.meta.env.VITE_DATA
 })
 const useAxiosSecure = () => {
-    const navigate = useNavigate();
-    const {logOut} = useAuth();
+    // const navigate = useNavigate();
+    // const {logOut} = useAuth();
     axiosSecure.interceptors.request.use(function(config){
         const token = localStorage.getItem('access-token')
-        console.log(token);
         config.headers.authorization = `Bearer ${token}`;
         return config;
     }, function(error){
@@ -22,10 +21,10 @@ const useAxiosSecure = () => {
         return response;
     }, async (error) => {
         const status = error.response.status;
-        console.log(status);
         if(status === 401 || status === 403){
-            await logOut();
-            navigate('/login')
+            // await logOut();
+            // navigate('/login')
+
         }
         return Promise.reject(error);
     })
